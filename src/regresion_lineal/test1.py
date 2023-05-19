5cde108325d208ea8c33f47010efedce9e83860e
Marto Nieto Guerrero <marto.nieto.g16@gmail.com>
2023-05-19 15:41:35 -0500

    test1.py

--- src/regresion_lineal/test1.py
+++ src/regresion_lineal/test1.py
@@ -6,24 +6,36 @@ import matplotlib.pyplot as plt
 
 symbol = "BTC-USD"
 data = yf.download(symbol, start="2020-12-01", end="2023-05-15")
-df = pd.DataFrame(data['Close'])
-df.reset_index(inplace=True)
 
-# Convertir la columna de fechas a un tipo de datos compatible con float64
-X_train = (df['Date'][:int(len(df)*0.8)].values.astype(np.int64) // 10**9).reshape(-1, 1)
-y_train = df['Close'][:int(len(df)*0.8)].values.reshape(-1,1)
+# Extraer los precios de cierre y fechas en un DataFrame
+df = pd.DataFrame({'Date': data.index, 'Close': data['Close']})
 
-X_test = (df['Date'][int(len(df)*0.8):].values.astype(np.int64) // 10**9).reshape(-1, 1)
-y_test = df['Close'][int(len(df)*0.8):].values.reshape(-1,1)
+# Dividir los datos en entrenamiento y prueba
+train_size = int(len(df) * 0.8)
+train_data = df[:train_size]
+test_data = df[train_size:]
 
+# Convertir las fechas a un formato numérico compatible
+train_dates = (train_data['Date'].values.astype(np.int64) // 10**9).reshape(-1, 1)
+test_dates = (test_data['Date'].values.astype(np.int64) // 10**9).reshape(-1, 1)
+
+# Obtener los precios de cierre correspondientes
+train_prices = train_data['Close'].values.reshape(-1, 1)
+test_prices = test_data['Close'].values.reshape(-1, 1)
+
+# Crear y entrenar el modelo de regresión lineal
 reg = LinearRegression()
-reg.fit(X_train, y_train)
+reg.fit(train_dates, train_prices)
 
-y_pred = reg.predict(X_test)
+# Realizar la predicción
+predicted_prices = reg.predict(test_dates)
 
-plt.scatter(X_train, y_train, color='blue')
-plt.scatter(X_test, y_test, color='green')
-plt.plot(X_test, y_pred, color='red')
+# Visualizar los resultados
+plt.scatter(train_dates, train_prices, color='blue', label='Train Data')
+plt.scatter(test_dates, test_prices, color='green', label='Test Data')
+plt.plot(test_dates, predicted_prices, color='red', label='Prediction')
+plt.legend()
 plt.show()
 
-print(" A predicted price of: $", int(y_pred[0][0]) )
+# Imprimir el precio predicho más reciente
+print("A predicted price of: $", int(predicted_prices[-1][0]))
